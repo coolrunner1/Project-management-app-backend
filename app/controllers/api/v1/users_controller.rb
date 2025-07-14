bdd0c64c42a2d299295c30df75d98feddfb1d8e8
coolrunner1 <waurttttt@gmail.com>
2025-07-14 14:31:04 +0300

    Added user controller and model

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,53 @@
+class Api::V1::UsersController < ApplicationController
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def show
+        if @user
+            render json: @user
+        else 
+            render json: {"error": "not found"}, status: :not_found
+        end
+    end
+
+    def create
+        print user_params
+        @user = User.new(user_params)
+
+        if @user.save
+            render json: @user, status: :created
+        else 
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @user.update(user_params.except(:password))
+            if params[:user][:password].present?
+                @user.update(password: params[:user][:password])
+            end
+            render json: @user
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        if @user
+            @user.destroy
+        else 
+            render json: {"error": "not found"}, status: :not_found
+        end
+    end
+
+    private
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:username, :email, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   resources :temp
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
+
+  get "/404" => "errors#not_found"
+  get "/500" => "errors#exception"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
